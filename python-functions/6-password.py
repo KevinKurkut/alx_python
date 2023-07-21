76ec598421ece12aefc370e1c4f7214674fc2697
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-22 00:03:40 +0400

    password validation

---
+++ python-functions/6-password.py
@@ -0,0 +1,10 @@
+def validate_password(password):
+    if len(password) < 0:
+        return False
+    elif not any(char.isupper() for char in password):
+        return False
+    elif not any(char.islower() for char in password):
+        return False
+    elif not any(char.isdigit() for char in password):
+        return False
+    
