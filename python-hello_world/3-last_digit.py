3aebf4b416e974f2700c0fa67819d604bacf07f5
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-21 14:35:11 +0400

    last digit update43

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,6 +9,6 @@ elif number < 0:
     if last_digit > 5:
         print("and is greater than 5")
     elif last_digit == 0:
-        print("and iis 0")
-    elif last_digit<6 and last_digit!=0:
+        print("and is 0")
+    elif last_digit < 6 and last_digit != 0:
         print("and is less than 6 and not 0")
