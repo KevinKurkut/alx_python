257a0603cd6df4250641919aac360ed3341ef269
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-24 16:53:38 +0400

    first commit

--- python-import_modules/0-import_add.py
+++ python-import_modules/0-import_add.py
@@ -1,6 +1,5 @@
-# Import the add function from the add_0.py file
 from add_0 import add
 a = 1
 b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+value = add(a, b)
+print("{} + {} = {}".format(a, b, value))
--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,4 +1,2 @@
 def add(a, b):
-    a = 1
-    b = 2
-    return ( a + b )
+    return (a + b)
