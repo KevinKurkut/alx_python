159dcfe47a587b8108dac8f1cbaf345b439a48f4
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-25 22:04:54 +0400

    everything can be imported3

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,6 @@
-from variable_load_2 import a
+import variable_load_2
 def print_value():
-    print("value is {}".format(a))
-
+    
     if __name__ == "__main__":
         print_value()
         
