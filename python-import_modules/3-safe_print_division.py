0039aacccafe2f7910d921c290d143a338f83685
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-26 14:53:29 +0400

    safe division4

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,10 +1,16 @@
 def safe_print_division(a, b):
+    a=24
+    b=0
    
     try:
-        result = a / b
+        result = safe_print_division(a, b)
     except ZeroDivisionError:
         result = None
+        print("Inside result: ".format(result))
+        print("{:d} / {:d} = {}".format(a, b, result))
     finally:
         print("Inside result: ".format(result))
         print("{:d} / {:d} = {}".format(a, b, result))
+       
+
   
