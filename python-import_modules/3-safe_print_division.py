c18f12f177db68654675fd2626237a3867dc48e3
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-26 15:19:06 +0400

    division by zero safe

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,14 +1,18 @@
 def safe_print_division(a, b):
-    a=24
-    b=0
    
     try:
         result = (a / b)
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: ".format(result))
+        print("Inside result: {}".format(result))
         print("{:d} / {:d} = {}".format(a, b, result))
+        return result
+    
+safe_print_division(12, 2)
+safe_print_division(12, 0)
+
+     
        
 
   
