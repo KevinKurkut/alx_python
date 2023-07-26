b33e89b6f4e8a8a04d70ddd00bd72a455aa6889d
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-26 16:19:38 +0400

    initial

---
+++ python-import_modules/4-raise_exception.py
@@ -0,0 +1,7 @@
+def raise_exception():        
+        try:
+          raise_exception()
+        except TypeError as te:
+              print("Exception raised", te)
+              
+              
