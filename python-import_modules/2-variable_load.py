adaf1b209912985d498e73eebbf01937a23a4149
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-26 20:06:14 +0400

    updated

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,7 +1,3 @@
 from variable_load_2 import a
-def main():
-    print("{}".format(a))
-    if __name__ == "__main__":
-        main()
-    print("{}".format(-100))
-    print("{}".format(98)) 
+print(a)
+   
