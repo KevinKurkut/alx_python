927ab35e7c68913ff6fa3b01cabc904fd387b5e4
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-26 15:23:17 +0400

    division by zero safe3

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
     finally:
         print("Inside result: {}".format(result))
         print("{:d} / {:d} = {}".format(a, b, result))
-        return result
+        
     
 
 
