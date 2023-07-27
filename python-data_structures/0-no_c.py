cbd3d93ee471d626446a3df8ebfa893ae6bb27ea
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-28 00:21:26 +0400

    fixed

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,6 @@
 def no_c(my_string):
     new_string=""
-    for char in range(len(my_string)):
-        if my_string[char] != "C" and my_string[char] != "C":
-            new_string = new_string + my_string[char]
-            return my_string
+    for char in my_string:
+        if char not in ( "c", "C"):
+            new_string += char
+            return new_string
