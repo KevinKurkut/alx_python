243c6b0b74b9575859e7bda8fd97587d788c71c3
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-07-27 23:07:15 +0400

    first commit

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,6 @@
+def no_c(my_string):
+    new_string=""
+    for char in range(len(my_string)):
+        if my_string[char] != "C" and my_string[char] != "C":
+            new_string = new_string + my_string[char]
+            return my_string
