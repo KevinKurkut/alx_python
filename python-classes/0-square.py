f4ca405617651799c246c59eeb03f6387388e856
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-02 14:16:01 +0400

    documennted

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!usr/bin/python3
+"""module with a class that defines a square"""
+class Square:
+    """defining square object
+    args:
+    size(param
+    ) is assumed to be an int"""
+    def __init__(self, size):
+            self.__size = size 
+            """private attribute"""
