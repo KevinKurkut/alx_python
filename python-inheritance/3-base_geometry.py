38157a041d846869e93c5fc8bd618eeac3d6f70c
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-04 00:03:24 +0400

    updated

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,16 @@
 """creating an empty class"""
 class BaseGeometry():
     """BaseGeometry class"""
-    pass
-    
+
+    """overiding default behaviour of the dir() method when printed"""
+    def __init__(cls):
+        """getting the attributes of parent class not awre of"""
+        attributes = super().__init__()
+        list_to_return = []
+        for attr in attributes:
+            if attr in attributes:
+                if attr != "__init_subclass__":
+                    #append to the list to return
+                    list_to_return.append(attr)
+            return list_to_return
+        
