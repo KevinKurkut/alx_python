c2c648bcd7a45d77e5a1e8f1c2f5744d6c6fd8b2
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-03 14:30:22 +0400

    fixed trailing whitespace

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,4 +2,5 @@
 that inherited (directly or indirectly)
  from the specified class ; otherwise False"""
 def inherits_from(obj, a_class):
+    """the object is an instance of a class"""
     return isinstance(obj, a_class) and type(obj) != a_class
