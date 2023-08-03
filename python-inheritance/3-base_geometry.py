7352c7e19688d47aa4925afd0d3a62b1a32cd851
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-03 23:44:19 +0400

    updated235

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,13 @@
 """creating an empty class"""
-class BaseGeometry():
+from collections.abc import Iterable
+
+
+class NoinitSubclassMeta(type):
+    def __dir__(cls):
+        return [attr for attr in super().__dir__()  if attr != "__ini_subclass__"]
+
+class BaseGeometry(metaclass = NoinitSubclassMeta):
     """BaseGeometry class"""
-    pass
-    
+    def __dir__(cls):
+        """removing init_subclass"""
+        return [attr for attr in super().__dir__() if attr != "__init_subclass__"]
