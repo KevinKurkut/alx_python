5f6924c770918a0b2df04b94c0cd3e5176095bcc
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-03 14:32:20 +0400

    fixed trailing whitespace2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,6 +1,4 @@
-""" function that returns True if the object is an instance of a class 
-that inherited (directly or indirectly)
- from the specified class ; otherwise False"""
+""" function that returns True if the object"""
 def inherits_from(obj, a_class):
     """the object is an instance of a class"""
     return isinstance(obj, a_class) and type(obj) != a_class
