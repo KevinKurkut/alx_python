dd6d3bc68246bb7e3cff8a8824b6c9aa175bad13
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-03 14:36:06 +0400

    fixed trailing whitespace256

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,4 +1,5 @@
-""" function that returns True if the object"""
+"""function that returns True if the object"""
 def inherits_from(obj, a_class):
+    """the object is an instance of a class"""
     return isinstance(obj, a_class) and type(obj) != a_class
-"""the object is an instance of a class"""
+
