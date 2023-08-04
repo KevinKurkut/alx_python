525a9b221351b2d34dcd7f28337cb8a1f6c3fbfc
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-04 22:07:47 +0400

    resolved

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,10 @@
-"""creating an empty class"""
-class BaseGeometry:
-    """BaseGeometry class"""
-    
+"""empty class"""
+class parent(type):
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+class BaseGeometry(metaclass = parent):
+    """BaseGeometry class"""  
     def __dir__(cls) -> None:
         """get attributes for this class"""
         attributes = super().__dir__()
