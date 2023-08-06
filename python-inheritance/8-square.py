4d63121a5d1dcf7c527f3144e0b66572efea7afa
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-06 14:41:36 +0400

    pep validate

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -37,20 +37,15 @@ class Rectangle(BaseGeometry):
         """area() method  implemented"""
     def area(self):
         return self.__width * self.__height
-
 """ class Square that inherits from Rectangle"""
 class Square(Rectangle):
     """Square that inherits from Rectangle"""
     def __init__(self, size):
-
         """validate using integer validator"""
         self.integer_validator("size", size)
         self.__size = size
     def __str__(self):
         return super().__str__()
-    
         """calculate area of Square"""
     def area(self):
         return self.__size ** 2
-
-
