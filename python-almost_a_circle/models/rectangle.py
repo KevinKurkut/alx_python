e960cbe10fb01373cc674e188fbbd736e85fb34c
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-09 15:50:04 +0400

    finishwd1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,10 +8,10 @@ class Rectangle(Base):
    args:
     self, width, height, x=0, y=0, id=None """
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y 
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y 
         """Call the parent class constructor"""
         super().__init__(id)
         @property
