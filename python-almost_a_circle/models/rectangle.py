17569277087ed3d20dab978f71a980f56a1dfe38
Kevin <123396179+KevinKurkut@users.noreply.github.com>
2023-08-10 17:23:31 +0400

    Update rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -22,28 +22,12 @@ class Rectangle(Base):
         @width.setter
         def width(self, value):
             """set wiidth"""
-            while type(value) is int:
-                  if value < 0:
-                        raise ValueError("width must be >= 0")
-                  break
-            else:           
-                raise TypeError("width must be an integer")
-            self.__width = value
-
         @property
         def height(self):
             """accessing height"""
             return self.__height
         @height.setter
-        def height(self, value):
-            """adding validation of all setter method"""
-            while type(value) is int:
-                  if value < 0:
-                        raise ValueError("height must be >= 0")
-                  break
-            else:           
-                raise TypeError("height must be an integer")
-            
+        def height(self, value):        
             """setting heigth"""
             self.__height = value
 
@@ -54,12 +38,6 @@ class Rectangle(Base):
         @x.setter
         def x(self, value):
             """set x"""
-            while type(value) is int:
-                  if value < 0:
-                        raise ValueError("x must be >= 0")
-                  break
-            else:           
-                raise TypeError("x must be an integer")
             self.__x = value
 
         @property
@@ -69,10 +47,5 @@ class Rectangle(Base):
         @y.setter
         def y(self, value):
             """access y"""
-            while type(value) is int:
-                  if value < 0:
-                        raise ValueError("y must be >= 0")
-                  break
-            else:           
-                raise TypeError("y must be an integer")
             self.__y = value
+            
