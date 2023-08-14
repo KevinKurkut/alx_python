ace005d2911801b4d71292423991b02316c0b94c
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-14 18:21:26 +0400

    orime

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,11 +1,9 @@
 def is_prime(number):
-    if number > 1:
-        """iterate all numbers till the number"""
-        for m in range(2, number):
-            if number % m == 0:
-                return False #is not prime
-            else:
-                return True #prime number
-    else:
-        return False    
-            
+    if number <= 1:
+        return False
+    lower_prime = 2
+    while lower_prime < number:
+        if number % lower_prime == 0:
+            return False
+        lower_prime += 1
+    return True
