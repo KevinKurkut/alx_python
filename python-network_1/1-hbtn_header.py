b15ecf026cb563089d960e0563e27500b273b667
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-15 18:20:58 +0400

    updatted

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,7 @@
 """import  requests"""
 import requests
+"""import sys module"""
+import sys
 """Write a Python script that takes in a URL, sends a request to the URL and 
 displays the value of the variable X-Request-Id in the response header"""
 response = requests.get('https://alu-intranet.hbtn.io/status')
