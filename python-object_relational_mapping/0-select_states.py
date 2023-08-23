6edb8fdea09c7cc1e1e62702b67cb0c7d5a9f66f
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-23 23:14:32 +0300

    initialize

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,18 @@
 """MySQLdb is an interface for connecting to 
 a MySQL database server from Python."""
 import MySQLdb
+from sys import argv
 """open database communication"""
-db = MySQLdb.connect("localhost", "root", "Chebiemitform3", "hbtn_0e_0_usa")
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 # prepare a cursor object using cursor() method
-cursor = db.cursor()
+cur = conn.cursor()
 # execute SQL query using execute() method
 query = "SELECT * FROM states ORDER BY id ASC;"
-cursor.execute(query)
+cur.execute(query)
 """# Fetch a rows """
-results = cursor.fetchall()
+results = cur.fetchall()
 for row in results:
     print(row)
-cursor.close()
-db.close()
+    """close both database and cursor"""
+cur.close()
+conn.close()
