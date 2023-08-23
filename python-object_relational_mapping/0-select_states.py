7598ccd707174c4924448a86dfe63e5bf26866db
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-23 18:14:30 +0300

    first

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 a MySQL database server from Python."""
 import MySQLdb
 """open database communication"""
-db = MySQLdb.connect("localhost","root","Chebiemitform3","hbtn_0e_0_usa" )
+db = MySQLdb.connect("localhost", "root", "Chebiemitform3", "hbtn_0e_0_usa")
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
 """sql statement
