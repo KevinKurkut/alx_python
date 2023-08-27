baaab821d25652bfe65f487c2c3fdaaa83bdcfcf
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-27 22:35:41 +0300

    updated

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,9 +1,14 @@
-"""Start link class to table in database"""
-import sys
-from model_state import Base, State
-
-from sqlalchemy import (create_engine)
-
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+from sqlalchemy import column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+"""
+class States
+id int NOT NULL AUTO_INCREMENT
+name varchar(128)
+PRIMARY KEY (`id`)
+"""
+class States(Base):
+    __tablename__ = 'states'
+    id = column(Integer(), primary_key=True, nullable=False)
+    name = column(String(128), nullable=False)
+    
