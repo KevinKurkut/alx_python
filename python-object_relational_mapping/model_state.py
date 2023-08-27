04fbfc1a2dcb008068634b7181ea85c89d1d57e9
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-27 22:48:08 +0300

    second

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,14 +1,13 @@
-from sqlalchemy import column, String, Integer
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
-"""
-class States
-id int NOT NULL AUTO_INCREMENT
-name varchar(128)
-PRIMARY KEY (`id`)
+"""class States
+id int primary auto increment
+name
 """
 class States(Base):
     __tablename__ = 'states'
-    id = column(Integer(), primary_key=True, nullable=False)
-    name = column(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name =  Column(String(128), nullable=False)
     
