3817bb9e78bc57b6c16677e02d11fcdf2f45f711
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-08-30 23:09:31 +0300

    documented

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,11 +1,11 @@
+"""importing flask module"""
 from flask import Flask
-
+"""create an instance"""
 app = Flask(__name__)
-
+"""route definitiion"""
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
