d364f46300ff25bad4b56f2d83336a9a8a814ec3
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-09-02 15:02:30 +0300

    updated

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,10 @@
-from sys import argv
-if __name__ == "__main__":
-    def print_arguments(args):
-        number = len(argv)
-        if number==0:
-            print("{} argument")
-        elif number==1:
-            print("{} arguments.")
-            print("{}: {}".format(number, args))
-
-        
-        
-        
+import sys
+"""subsequent elements from index 1"""
+args = sys.argv[1:]
+argument_number = len(args)
+if argument_number == 0:
+    print("0 arguments.")
+else:
+    print(f"{argument_number} {'argument' if argument_number == 1 else 'arguments'}:")
+    for i, arg in enumerate(args, 1):
+        print(f"{i}: {arg}")
