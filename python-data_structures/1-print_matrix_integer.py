0c6da0583c250770cc9a7740e3a749dfd2248a8b
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-01 13:34:59 +0300

    lists of lists

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for number in row:
-            print("{:d}".format(number), end=' ')
-        print()
+        print(" ".join(map(str, row)))
