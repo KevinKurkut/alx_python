9b344aa1bbbed6c548d2da6bc2a488e0def291d6
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-01 22:29:05 +0300

    sets

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,7 +1,2 @@
 def common_elements(set_1, set_2):
-    c_set=set()
-    for element in set_1:
-        if element in set_2:
-            c_set.add(element)
-            return c_set
-        
+    return set_1.intersection(set_2)
