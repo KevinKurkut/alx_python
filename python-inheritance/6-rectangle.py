308bf31b922f4c5257b691ceb06ade45a3cb2043
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-03 14:07:30 +0300

    documented

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,7 +1,14 @@
+#!/usr/bin/python3
+"""
+create rectangle class
+"""
 BaseGeometry =__import__('5-base_geometry').BaseGeometry
         
 class Rectangle(BaseGeometry):
-    """inherits all property of base geometry"""
+    """inherits all property of base geometry
+    width
+    height
+    """
     def __init__(self, width, height):
         """Rectangle class will no longer inherit the parent's __init__() function"""
         self.integer_validator("width", width)
