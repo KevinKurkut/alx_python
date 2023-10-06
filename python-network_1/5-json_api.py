403147a8f60c3797bf281646c1f84c23f2c725ae
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-06 22:45:06 +0300

    fixed

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,21 +1,16 @@
 import requests
 import sys
-
-def search(letter):
-    url = "http://0.0.0.0:5000/search_user"
-    params = {'q': letter}
-    response = requests.post(url, params=params)
-
-    try:
-        response.raise_for_status()
-        user_data = response.json()
-        if user_data:
-            print(f"[{user_data['id']}] {user_data['name']}")
-        else:
-            print("No result")
-    except (requests.exceptions.HTTPError, ValueError):
-        print("Not a valid JSON")
-
 if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    search(letter)
+    newData = {
+        "letter": "q"
+    }
+    url_post = "http://0.0.0.0:5000/search_user"
+    post_response = requests.post(url_post, json=newData)
+    # Print the response
+    if post_response is None:
+        print("Not a valid JSON")
+    elif not post_response:
+        print("No result")
+    else:
+        print("[{}] {}".format(post_response['id'], post_response['name']))
+    
