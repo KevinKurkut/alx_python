a86bc2f189d4e07da8419c709701ee3898ef2c27
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-06 22:25:24 +0300

    Search API

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 
-def search_user_by_letter(letter):
+def search(letter):
     url = "http://0.0.0.0:5000/search_user"
     params = {'q': letter}
     response = requests.post(url, params=params)
@@ -18,4 +18,4 @@ def search_user_by_letter(letter):
 
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    search_user_by_letter(letter)
+    search(letter)
