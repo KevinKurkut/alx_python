17719bf134b63423feba076ae5cd3ee0cbf4151e
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-06 22:11:13 +0300

    fixed

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -2,7 +2,7 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
-    url = "http://api.github.com/user"
+    url = "https://api.github.com/user"
     username = sys.argv[1]
     password = sys.argv[2]
     response = requests.get(url, auth=HTTPBasicAuth(username, password))
