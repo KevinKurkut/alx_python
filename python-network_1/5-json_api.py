9929565a95a42feae6d3920ca042a4758713b875
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-08 10:58:08 +0300

    Search API

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,16 +1,21 @@
 import requests
 import sys
 if __name__ == "__main__":
-    newData = {
-        "letter": "q"
-    }
-    url_post = "http://0.0.0.0:5000/search_user"
-    post_response = requests.post(url_post, json=newData)
-    # Print the response
-    if post_response is None:
-        print("Not a valid JSON")
-    elif not post_response:
-        print("No result")
+    if len(sys.argv)>1:
+        q=sys.argv[1]
     else:
-        print("[{}] {}".format(post_response['id'], post_response['name']))
+        q=""
+    url_post = "http://0.0.0.0:5000/search_user"    
+    newData={"q": q}
+    post_response = requests.post(url_post, json=newData)
+    try:
+        json =post_response.json()
+        if not json:
+            print("No result")
+        else:
+            id= json.get("id")
+            name= json.get("namw")
+            print("[{}] {}".format(id, name))
+    except ValueError as e:
+        print("Not a valid JSON")
     
