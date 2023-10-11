8fd50425641952168ee9c2ed9ec75bb61e257571
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-10-11 17:43:35 +0300

    first model

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,16 +1,21 @@
+#!/usr/bin/python3
+"""
+Python file that contains the class definition of a State and an instance
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
 
 Base = declarative_base()
-"""First state model
-table name is states"""
+
 class State(Base):
-    __tablename__ = 'states'
+    """
+    State class that inherits from Base
+
+    Attributes:
+        id: Id
+        name: Name
+    """
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
-
-engine = create_engine('mysql://root:root@localhost:3306/hbtn_0e_6_usa')
-
-if __name__ == '__main__':
-    Base.metadata.create_all(engine)
+    name = Column(String(128), nullable=False
+                  
