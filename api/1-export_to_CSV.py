fc623c8c942c2f8b849078c2d11c188f57cf97e8
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-11-03 10:39:08 +0300

    1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,39 +1,50 @@
 #!/usr/bin/python3
-"""import necessary modules"""
-
 import csv
 import requests
 import sys
 
-def export_to_CSV(user_id):
-    employee_name = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id)).json()["name"]
-    tasks = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id)).json()
 
-    tasks_data = []
+def get_employee_data(employee_id):
+    # Define the API endpoints
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    for task in tasks:
-        tasks_data.append(
-            [
-                str(user_id),
-                employee_name,
-                task["completed"],
-                task["title"],
-            ]
-        )
+    try:
+        # Fetch employee data
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+        user_id = user_data.get('id')
+        username = user_data.get('username')
 
-    with open(str(user_id) + ".csv", "w", encoding="UTF8", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(tasks_data)
+        # Fetch TODO list data
+        todos_response = requests.get(todos_url)
+        todos_data = todos_response.json()
+
+        # Create a CSV file for the employee
+        csv_filename = f"{user_id}.csv"
+        with open(csv_filename, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+
+            # Write header row
+            csv_writer.writerow(
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+            # Write task data to the CSV
+            for task in todos_data:
+                csv_writer.writerow(
+                    [user_id, username, task["completed"], task["title"]])
+
+        print(f"Data exported to {csv_filename}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py EMPLOYEE_ID")
+        print("Usage: python script.py <employee_id>")
         sys.exit(1)
 
-    try:
-        employee_id = int(sys.argv[1])
-        export_to_CSV(employee_id)
-    except ValueError:
-        print("Please provide a valid employee ID.")
-        
+employee_id = int(sys.argv[1])
+get_employee_data(1)
