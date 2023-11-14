1d85cdf1ac25004ee90130837e29b9b58f0a4501
kevinkurkut@gmail.com <kevinkurkut@gmail.com>
2023-11-14 12:09:01 +0300

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,20 +1,44 @@
-#!/usr/bin/python3
-"""impoert necessary modules"""
-from json import dump
-from requests import get
-from sys import argv
+import requests
+import sys
+import json
+
+def tasks(user_id):
+    user_info = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
+    tasks_request = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}/todos')
+
+    if user_info.status_code != 200 or tasks_request.status_code != 200:
+        print(f"Error: Unable to retrieve data for user {user_id}")
+        return None
+
+    user_info = json.loads(user_info.text)
+    tasks = json.loads(tasks_request.text)
+
+    for task in tasks:
+        task["username"] = user_info["username"]
+
+    return tasks
+
+def export_to_json(user_id, tasks):
+    if not tasks:
+        print(f"No tasks found for user {user_id}")
+        return
+
+    filename = f"{user_id}.json"
+
+    data = {str(user_id): [{"task": task["title"], "completed": task["completed"], "username": task["username"]} for task in tasks]}
+
+    # Write to JSON file
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+    print(f"Data exported to {filename}")
+
 if __name__ == "__main__":
-    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(argv[1])
-    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <USER_ID>")
+        sys.exit(1)
 
-    todo_list = []
-    for todo in todo_result:
-        todo_dict = {}
-        todo_dict.update({ "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {
-            "task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]})
-        todo_list.append(todo_dict)
+    user_id = sys.argv[1]
+    user_tasks = tasks(user_id)
 
-    with open("USER_ID.json".format(argv[1]), 'w') as f:
-        dump({argv[1]: todo_list}, f)
+    export_to_json(user_id, user_tasks)
